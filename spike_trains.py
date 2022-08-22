8110f11bf4bbe5d3dec8d5734f1f13843ecc92cd
melissafasol <melissaclairefasol@gmail.com>
2022-08-22 20:49:21 +0200

    NMDA receptor

---
+++ spike_trains.py
@@ -0,0 +1,56 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def poisson_spikes(t1, t2, N, rate=10, dt=0.1):
+  """
+  Poisson spike generator.
+  Parameters
+  ----------
+  t1 : float
+      Simulation time start in miliseconds (ms).
+  t2 : float
+      Simulation time end in miliseconds (ms).
+  N : int, optional
+      Number of presynaptic spikes.
+  rate : float, optional
+      Mean firing rate in Hz. The default is 10.
+  dt : float, optional
+      Time step in ms. The default is 0.1.
+  Returns
+  -------
+  spks : TYPE
+      DESCRIPTION.
+  """
+  spks = []
+  tarray = np.arange(t1, t2+dt, dt)
+  for n in range(N):
+    spkt = tarray[np.random.rand(len(tarray)) < rate*dt/1000.]  # Determine list of times of spikes
+    idx = [n]*len(spkt)  # Create vector for neuron ID number the same length as time
+    spkn = np.concatenate([[idx], [spkt]], axis=0).T  # Combine tw lists
+    if len(spkn) > 0:
+      spks.append(spkn)
+  spks = np.concatenate(spks, axis=0)
+  return spks
+
+pre_cells = 10000
+spks = poisson_spikes(t1=0, t2=1000, N=pre_cells, rate=25)
+
+plt.figure(figsize=(12, 8))
+for i, r in enumerate(np.random.choice(pre_cells, 100, replace=False)):
+  spks_i = spks[spks[:, 0] == r][:,1]
+  plt.scatter(spks_i, i*np.ones((len(spks_i), )), color='k')
+plt.ylabel('neuron id')
+plt.xlabel('time (ms)')
+plt.show()
+
+freqs = []
+for i in range(pre_cells):
+  spks_i = spks[spks[:, 0] == i][:, 1]
+  freqs.append(len(spks_i))
+
+plt.figure(figsize=(12, 8))
+plt.hist(freqs, bins=20)
+plt.ylabel('neuron id')
+plt.xlabel('time (ms)')
+plt.show()
--- stimulation.py
+++ stimulation.py
@@ -36,7 +36,7 @@ ncon_2 = h.NetCon(cell_3.soma(0.5)._ref_v, syn_3, sec= cell_3.soma)
 ncon_2_NMDA = h.NetCon(cell_3.soma(0.5)._ref_v, syn_2_NMDA, sec = cell_3.soma)
 ncon_3 = h.Netcon(cell_4.soma(0.5)._ref_v, syn_2, sec = cell_4.soma)
 ncon_3_NMDA = h.NetCon(cell_4.soma(0.5)._ref_v, syn_3_NMDA, sec = cell_4.soma)
-
+ncon_4 = h.Netcon(cell_0.soma(0.5)._ref_v, syn_4, sec = cell_0.soma)
 #should cell 5 and cell 0 be connected through synapse?
 
 #spike threshold to decide whether spike will be transmitted to the next cell 
@@ -48,6 +48,7 @@ ncon_2.threshold = -10
 ncon_2_NMDA.threshold = -10
 ncon_3.threshold = -10
 ncon_3_NMDA.threshold = -10
+ncon_4.threshold = -10
 
 #delay of spike to next cell 
 weight_NMDA = 0.03
@@ -69,6 +70,7 @@ ncon_3.delay = 0
 ncon_3_NMDA.delay = 20
 ncon_3.weight[0] = weight_syn
 ncon_3_NMDA.weight[0] = weight_NMDA
+ncon_4.weight[0] = weight_syn
 
 # Create a current Clamp starting at 200 ms and with duration 5ms. Amplitude is an argument.     
 ic = h.IClamp(cell_0.soma(0.5))
@@ -80,33 +82,37 @@ ic.amp = 0.2  # nA
 syn_0 = h.Exp2Syn(cell_0.apic1(0.5))
 syn_0.tau1 = 0.1  # rise time #if synapse is NMDA tau1 = 10
 syn_0.tau2 = 20  # decay time #if synapse is NMDA tau2 = 75ms - because takes much more current and time for magnesium ion to be removed from channel
-syn_0.e = 0  # reversal potential of the synapse
+syn_0_NMDA.tau1 = 10
+syn_0_NMDA.tau2 = 75 
+syn_1 = h.ExpSyn(cell_1.apic1(0.5))
+syn_1_NMDA = h.NMDA(cell_1.tuft(0.5))
+syn_2 = h.ExpSyn(cell_2.apic1(0.5))
+syn_2_NMDA = h.NMDA(cell_2.tuft1(0.5))
+syn_3 = h.ExpSyn(cell_3.apic1(0.5))
+syn_3_NMDA = h.NMDA(cell_3.apic1(0.5)) 
+syn_4 = h.ExpSyn(cell_4.tuft1(0.5))
+syn_4_NMDA = h.NMDA(cell_4.apic1(0.5))
 
-syn_0_NMDA = h.NMDA
+syn_0.e = 0  # reversal potential of the synapse
 
 
 #========== ...create an artificial spike (an "event" to be delivered to the synapse)...
 tsignal = 20
-ns = h.NetStim(0.5)
+ns = h.NetStim(0.5) # do you input the spike vector here?
 ns.start = 200  # stimulus onset
 ns.number = 1  # number of events
 
 #... and connect the event to the synapse.
-nc = h.NetCon(ns, syn)
+nc = h.NetCon(ns, syn_0)
 nc.delay = 0  # synaptic delay (ms)
 nc.weight[0] = 0.002  # synaptic weight (strength) - importance of synapse in overall potential 
 
 
-cell_0_soma_vec = h.Vector().record(cell_0.soma(0.5)._ref_v)  #membrane potential vector
-cell_0_trunk3_vec = h.Vector().record(cell_0.trunk3(0.5)._ref_v)  
+cell_0_soma_vec = h.Vector().record(cell_0.soma(0.5)._ref_v)  #membrane potential vector 
 cell_1_soma_vec = h.Vector().record(cell_1.soma(0.5)._ref_v)  
-cell_1_trunk3_vec = h.Vector().record(cell_1.trunk3(0.5)._ref_v)  
 cell_2_soma_vec = h.Vector().record(cell_2.soma(0.5)._ref_v)  
-cell_2_trunk3_vec = h.Vector().record(cell_2.trunk3(0.5)._ref_v)  
 cell_3_soma_vec = h.Vector().record(cell_3.soma(0.5)._ref_v)  
-cell_3_trunk3_vec = h.Vector().record(cell_3.trunk3(0.5)._ref_v) 
 cell_4_soma_vec = h.Vector().record(cell_4.soma(0.5)._ref_v)  
-cell_4_trunk3_vec = h.Vector().record(cell_4.trunk3(0.5)._ref_v)  
 
 t_vec = h.Vector().record(h._ref_t)  # Time stamp vector
 
@@ -131,15 +137,15 @@ t_vec.remove(0, int(tremove/h.dt))
 
 plt.figure(figsize=(8, 6))
 plt.plot(t_vec, cell_0_soma_vec, label='soma 0 ')
-plt.plot(t_vec, cell_0_trunk3_vec, label='trunk3 0')
+plt.plot(t_vec, cell_0_trunk3_vec, label='dendrite 0')
 plt.plot(t_vec, cell_1_soma_vec, label='soma 1')
-plt.plot(t_vec, cell_1_trunk3_vec, label='trunk3 1')
+plt.plot(t_vec, cell_1_trunk3_vec, label='dendrite 1')
 plt.plot(t_vec, cell_2_soma_vec, label='soma 2 ')
-plt.plot(t_vec, cell_2_trunk3_vec, label='trunk3 2')
+plt.plot(t_vec, cell_2_trunk3_vec, label='dendrite 2')
 plt.plot(t_vec, cell_3_soma_vec, label='soma 3')
-plt.plot(t_vec, cell_3_trunk3_vec, label='trunk3 3')
+plt.plot(t_vec, cell_3_trunk3_vec, label='dendrite 3')
 plt.plot(t_vec, cell_4_soma_vec, label='soma 4')
-plt.plot(t_vec, cell_4_trunk3_vec, label='trunk3 4')
+plt.plot(t_vec, cell_4_trunk3_vec, label='dendrite 4')
 
 plt.xlabel('time (ms)')
 plt.ylabel('mV')
